093a23182b10f94d52ea9b39967a071d711ea6bd
Jhonatan Silva <jhonatan.silva@extreme.digital>
2022-11-04 12:44:34 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const audio = new Audio("https://www.fesliyanstudios.com/play-mp3/387");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    audio.play();
+  });
+});
